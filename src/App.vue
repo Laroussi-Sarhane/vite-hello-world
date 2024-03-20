c65243e6eeb6074175a9f8653507c998830bb5d1
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-03-20 15:15:06 +0100

    add

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,27 @@ export default {
   data(){
     return{
 
-      title: 'hello world'
+      title: 'hello world',
+      apiUrl:'https://flynn.boolean.careers/exercises/api/random/mail',
+      risultato: '',
+      emails: [],
     }
+  },
+
+  methods:{
+
+    getApi(){
+      for(let i = 0; i < 10; i++){
+        axios.get(this.apiUrl)
+        .then((risultato) => {
+          this.emails.push(risultato.data.response)
+        })
+      }
+    }
+  },
+
+  mounted(){
+    this.getApi()
   }
 }
 </script>
